24c68b1a0be0a6341007d7c480f77e62be484230
athulkkorom <athulkkorom@gmail.com>
2023-09-10 16:04:37 +0530

    main

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,10 +3,10 @@ import myImg from '../Assets/Athul.jpg'
 import {BiRightArrowAlt} from 'react-icons/bi'
 function Home() {
   return (
-    <div name='Home' className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row text-white'>
+    <div name='Home' className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
+        <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row text-white pt-20'>
             <div className=' flex flex-col justify-center items-center'>
-                <h1 className=' text-4xl sm:text-7xl'>I'm a Full Stack Developer</h1>
+                <h1 className=' text-4xl md:text-7xl'>I'm a Full Stack Developer</h1>
                 <p className=' text-gray-500 py-4'>A full-stack developer with passion and self-taught training who is able to pick up and use the newest technologies a strong work ethic
  and sense of duty to monitor and assess progress until the task at hand is finished. possess a strong dedication to the team atmosphere
  and take pleasure in working both independently and as a team member. works tenaciously and with strong passion to accomplish
